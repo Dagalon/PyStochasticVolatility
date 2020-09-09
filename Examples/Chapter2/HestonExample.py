c7a35bb1c985f2b3c39e19204e2ff965493c541c
Dagalon <david.garcia.lorite@outlook.com>
2020-09-09 18:00:52 +0200

    # Hestom paramaters example

--- Examples/Chapter2/HestonExample.py
+++ Examples/Chapter2/HestonExample.py
@@ -88,19 +88,19 @@ def get_smile_for_differents_k(rho: float,
     return heston_iv
 
 
-def get_smile_for_differents_v0(rho: float,
-                                epsilon: float,
-                                k: float,
-                                v0_s: Types.ndarray,
-                                theta: float,
-                                european_options: List[EuropeanOption]):
+def get_smile_for_differents_theta(rho: float,
+                                   epsilon: float,
+                                   k: float,
+                                   v0: float,
+                                   theta_s: Types.ndarray,
+                                   european_options: List[EuropeanOption]):
     heston_iv = []
-    no_v0_s = len(v0_s)
+    no_v0_s = len(theta_s)
     for i in range(0, no_v0_s):
-        v0_i = v0_s[i]
-        v0_i_iv = []
+        theta_i = theta_s[i]
+        theta_i_iv = []
         for option in european_options:
-            price = option.get_analytic_value(0.0, theta, rho, k, epsilon, v0_i, 0.0,
+            price = option.get_analytic_value(0.0, theta_i, rho, k, epsilon, v0, 0.0,
                                               model_type=Types.ANALYTIC_MODEL.HESTON_MODEL_REGULAR,
                                               compute_greek=False)
             if option._option_type == TypeEuropeanOption.CALL:
@@ -108,76 +108,111 @@ def get_smile_for_differents_v0(rho: float,
             else:
                 iv = implied_volatility(price, option._spot, option._strike, option._delta_time, 0.0, 0.0, 'p')
 
-            v0_i_iv.append(iv)
+            theta_i_iv.append(iv)
 
-        heston_iv.append((k_i, v0_i_iv))
+        heston_iv.append((k_i, theta_i_iv))
+
+    return heston_iv
+
+
+def get_smile_for_differents_k(rho: float,
+                               epsilon: float,
+                               k_s: Types.ndarray,
+                               v0: float,
+                               theta: float,
+                               european_options: List[EuropeanOption]):
+    heston_iv = []
+    no_k_s = len(k_s)
+    for i in range(0, no_k_s):
+        k_i_iv = []
+        for option in european_options:
+            price = option.get_analytic_value(0.0, theta, rho, k_s[i], epsilon, v0, 0.0,
+                                              model_type=Types.ANALYTIC_MODEL.HESTON_MODEL_REGULAR,
+                                              compute_greek=False)
+
+            if option._option_type == TypeEuropeanOption.CALL:
+                iv = implied_volatility(price, option._spot, option._strike, option._delta_time, 0.0, 0.0, 'c')
+            else:
+                iv = implied_volatility(price, option._spot, option._strike, option._delta_time, 0.0, 0.0, 'p')
+
+            k_i_iv.append(iv)
+
+        heston_iv.append((k_i, k_i_iv))
 
     return heston_iv
 
 
 # parameters
-epsilon = 1.1
-k = 0.5
+epsilon = 0.1
+k = 0.1
 v0 = 0.05
-theta = 0.05
+theta = 0.1
+rho = -0.75
 
 # options
-strikes = np.arange(50.0, 150.0, 10.0)
+strikes = np.arange(40.0, 200.0, 10.0)
+t_s = np.arange(0.25, 20, 0.5)
 f0 = 100
 T = 2.0
 notional = 1.0
-options = []
+options_strike = []
+options_time = []
 for k_i in strikes:
-    options.append(EuropeanOption(k_i, notional, TypeSellBuy.BUY, TypeEuropeanOption.CALL, f0, T))
+    options_strike.append(EuropeanOption(k_i, notional, TypeSellBuy.BUY, TypeEuropeanOption.CALL, f0, T))
+
+for t_i in t_s:
+    options_time.append(EuropeanOption(f0, notional, TypeSellBuy.BUY, TypeEuropeanOption.CALL, f0, t_i))
+
+# k effect in the smile
+k_s = [0.1, 0.5, 1.0, 2.0]
+style_markers = ['*', '.', 'x', '^']
+out_k_s = get_smile_for_differents_k(rho, epsilon, k_s, v0, theta, options_time)
+
+no_outputs = len(out_k_s)
+for i in range(0, no_outputs):
+    plt.plot(t_s, out_k_s[i][1], label="k="+str(k_s[i]), linestyle='dashed', color='black',
+             marker=style_markers[i])
+
+plt.legend()
+plt.show()
 
 # rho effect in the smile
-# rho_s = np.array([-0.9, -0.8, -0.7, -0.6, -0.5, -0.4, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
+# rho_s = np.array([-0.9, -0.5, -0.25, -0.0])
+# style_markers = ['*', '.', 'x', '^']
 #
-# out_rho_s = get_smile_for_differents_rho(rho_s, epsilon, k, v0, theta, options)
+# out_rho_s = get_smile_for_differents_rho(rho_s, epsilon, k, v0, theta, options_strike)
 #
 # no_outputs = len(out_rho_s)
 # for i in range(0, no_outputs):
-#     plt.plot(strikes, out_rho_s[i][1], label="rho="+str(rho_s[i]), linestyle='dashed')
+#     plt.plot(strikes, out_rho_s[i][1], label="rho="+str(rho_s[i]), linestyle='dashed', color='black',
+#              marker=style_markers[i])
 #
 # plt.legend()
 # plt.show()
 
 # epsilon effect in the smile
-# rho = -0.85
-# epsilon_s = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
-# out_epsilon_s = get_smile_for_differents_epsilon(rho, epsilon_s, k, v0, theta, options)
+# epsilon_s = np.array([0.1, 0.3, 0.5, 0.9])
+# style_markers = ['*', '.', 'x', '^']
+# out_epsilon_s = get_smile_for_differents_epsilon(rho, epsilon_s, k, v0, theta, options_strike)
 #
 # no_outputs = len(out_epsilon_s)
 # for i in range(0, no_outputs):
-#     plt.plot(strikes, out_epsilon_s[i][1], label="epsilon="+str(epsilon_s[i]), linestyle='dashed')
+#     plt.plot(strikes, out_epsilon_s[i][1], label="epsilon="+str(epsilon_s[i]), linestyle='dashed',
+#              color='black', marker=style_markers[i])
 #
 # plt.legend()
+# plt.title("Effect epsilon parameter")
 # plt.show()
 
-# k effect in the smile
-# rho = -0.85
-# epsilon = 0.8
-# k_s = np.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6])
-# out_k_s = get_smile_for_differents_k(rho, epsilon, k_s, v0, theta, options)
-#
-# no_outputs = len(out_k_s)
+
+# theta effect in the smile
+# theta_s = np.array([0.1, 0.07, 0.05, 0.03])
+# style_markers = ['*', '.', 'x', '^']
+# out_theta_s = get_smile_for_differents_theta(rho, epsilon, k, v0, theta_s, options_strike)
+# no_outputs = len(out_theta_s)
 # for i in range(0, no_outputs):
-#     plt.plot(strikes, out_k_s[i][1], label="k="+str(k_s[i]), linestyle='dashed')
+#     plt.plot(strikes, out_theta_s[i][1], label="theta=" + str(theta_s[i]), linestyle='dashed',
+#              color='black', marker=style_markers[i])
 #
 # plt.legend()
 # plt.show()
-
-# v0 effect in the smile
-rho = -0.85
-epsilon = 0.8
-k = 0.5
-v0_s = np.array([0.05, 0.06, 0.07, 0.08, 0.09, 0.1])
-out_v0_s = get_smile_for_differents_v0(rho, epsilon, k, v0_s, theta, options)
-no_outputs = len(out_v0_s)
-
-no_outputs = len(out_v0_s)
-for i in range(0, no_outputs):
-    plt.plot(strikes, out_v0_s[i][1], label="v0=" + str(v0_s[i]), linestyle='dashed')
-
-plt.legend()
-plt.show()
--- Examples/Chapter2/Remark20.py
+++ Examples/Chapter2/Remark20.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pylab as plt
 
 from functools import partial
-from MC_Engines.MC_LocalVolEngine import LocalVolFunctionals,LocalVolEngine
+from MC_Engines.MC_LocalVolEngine import LocalVolFunctionals, LocalVolEngine
 from scipy.interpolate import interp1d
 from Solvers.PDE_Solver import PDESolvers
 from Solvers.PDE_Solver import PDEOperators
@@ -26,7 +26,7 @@ def get_vetor_iv_cev(nu: float, alpha: float, T: float, f0: float):
     log_diffusion = partial(LocalVolFunctionals.log_cev_diffusion, beta=alpha - 1, sigma=nu)
     cev_pde = PDE.from_ipde_terms(LN_FORWARD_LOCAL_VOL_PDE(log_diffusion))
 
-    k_s = np.arange(f0 - 4.0, f0 + 4.0, 0.5)
+    k_s = np.arange(f0 - 3.0, f0 + 3.0, 0.5)
     # k_s = [f0]
     tc_s = [TerminalCondition(partial(f_ln_payoff, k=k_i)) for k_i in k_s]
     bc = Zero_Laplacian_BC()
@@ -61,24 +61,19 @@ def get_vetor_iv_cev(nu: float, alpha: float, T: float, f0: float):
     return z_s, iv_fd
 
 
-f0 = 10.0
-f0_up = 11.0
-f0_down = 9.0
 T = 1
 
 alpha = 0.3
 nu = 0.4
 
 # CEV parameter
-styles = ['o', '*', 'x']
-
-z_s, iv_fd = get_vetor_iv_cev(nu, alpha, T, f0)
-z_s_up, iv_fd_up = get_vetor_iv_cev(nu, alpha, T, f0_up)
-z_s_down, iv_fd_down = get_vetor_iv_cev(nu, alpha, T, f0_down)
-
-plt.plot(z_s, iv_fd, label="f0=" + str(f0), linestyle='--', marker=styles[0], color='black')
-plt.plot(z_s, iv_fd_up, label="f0=" + str(f0_up), linestyle='--', marker=styles[1], color='black')
-plt.plot(z_s, iv_fd_down, label="f0=" + str(f0_down), linestyle='--', marker=styles[2], color='black')
+styles = ['o', '*', 'x', '^', '.']
+f0_s = [10.0, 10.25, 10.5, 10.75, 11.0]
+no_f0 = len(f0_s)
+plt.figure()
+for i in range(0, no_f0):
+    z_s, iv_fd = get_vetor_iv_cev(nu, alpha, T, f0_s[i])
+    plt.plot(z_s, iv_fd, label="f0=" + str(f0_s[i]), linestyle='--', marker=styles[i], color='black')
 
 plt.legend()
 plt.show()
