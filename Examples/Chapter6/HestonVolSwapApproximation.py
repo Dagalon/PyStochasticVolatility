6a3f2d395e22410cb9332ba067e1d06f7429ef79
Dagalo <david.garcia.lorite@gmail.com>
2020-09-30 23:31:47 +0200

    # short lerm level impled vol

--- Examples/Chapter6/HestonVolSwapApproximation.py
+++ Examples/Chapter6/HestonVolSwapApproximation.py
@@ -3,13 +3,14 @@ import numpy as np
 
 from MC_Engines.MC_Heston import Heston_Engine
 from Tools import RNG, Types
-from AnalyticEngines.MalliavinMethod import ExpansionTools
+from Instruments.EuropeanInstruments import EuropeanOption, TypeSellBuy, TypeEuropeanOption
+from py_vollib.black_scholes_merton.implied_volatility import implied_volatility
+from scipy.optimize import curve_fit
 
-dt = np.arange(1, 11, 1) * 0.05
+dt = np.arange(7, 30, 1) * 1.0 / 365.0
 no_dt_s = len(dt)
 
 # simulation info
-f0 = 100.0
 epsilon = 0.5
 k = 0.5
 rho = -0.9
@@ -20,15 +21,25 @@ theta = 0.06
 parameters = [k, theta, epsilon, rho, v0]
 
 seed = 123456789
-no_paths = 200000
+no_paths = 300000
 delta_time = 1.0 / 365.0
 T = 0.25
 
 # random number generator
 rnd_generator = RNG.RndGenerator(seed)
 
+# option information
+f0 = 100.0
+options = []
+implied_vol_atm = []
+for d_i in dt:
+    options.append(EuropeanOption(f0, 1.0, TypeSellBuy.BUY, TypeEuropeanOption.CALL, f0, d_i))
+
+# outputs
 vol_swap_approximation = []
 vol_swap_mc = []
+implied_vol_atm = []
+output = []
 
 for i in range(0, no_dt_s):
     no_time_steps = int(dt[i] / delta_time)
@@ -36,11 +47,29 @@ for i in range(0, no_dt_s):
     map_output = Heston_Engine.get_path_multi_step(0.0, dt[i], parameters, f0, v0, no_paths,
                                                    no_time_steps, Types.TYPE_STANDARD_NORMAL_SAMPLING.ANTITHETIC,
                                                    rnd_generator)
-    vol_swap_approximation.append(ExpansionTools.get_vol_swap_approximation_heston(np.array(parameters), 0.0, dt[i], sigma_0))
+
+    mc_option_price = options[i].get_price(map_output[Types.HESTON_OUTPUT.PATHS][:, -1])
+    option_price = options[i].get_analytic_value(0.0, theta, rho, k, epsilon, v0, 0.0,
+                                                 model_type=Types.ANALYTIC_MODEL.HESTON_MODEL_ATTARI,
+                                                 compute_greek=False)
+
+    implied_vol_atm.append(implied_volatility(option_price, f0, f0, dt[i], 0.0, 0.0, 'c'))
     vol_swap_mc.append(np.sqrt(np.mean(np.sum(map_output[Types.HESTON_OUTPUT.INTEGRAL_VARIANCE_PATHS], 1)) / dt[i]))
+    output.append((implied_vol_atm[-1] - vol_swap_mc[-1]))
 
-plt.plot(dt, vol_swap_approximation, label='vol_swap_approximation')
-plt.plot(dt, vol_swap_mc, label='vol_swap_mc')
+# curve fit
 
+
+def f_law(x, a, b):
+    return a + b * x
+
+
+popt, pcov = curve_fit(f_law, dt, output)
+y_fit_values = f_law(dt, *popt)
+
+plt.plot(dt, output, label='(I(t,f0) - E(v_t) / t)', linestyle='--')
+plt.plot(dt, y_fit_values, label="%s + %s * t" % (round(popt[0], 5), round(popt[1], 5)), marker='.')
+
+plt.xlabel('t')
 plt.legend()
 plt.show()
