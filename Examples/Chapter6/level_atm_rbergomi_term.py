# fixed bug covariance matrix rbergomi

Examples/Chapter6/level_atm_rbergomi_term.py
@@ -13,9 +13,9 @@ dt = np.arange(1.0, 182, 2) * 1.0 / 365.0
 no_dt_s = len(dt)
 
 # simulation info
-h = 0.2
+h = 0.3
 nu = 0.5
-rho = -0.4
+rho = 0.0
 v0 = 0.05
 sigma_0 = np.sqrt(v0)
 
@@ -50,9 +50,9 @@ aux_output = []
 for i in range(0, no_dt_s):
     if dt[i] < 0.1:
         no_time_steps = 100
-        no_paths = 1000000
+        no_paths = 500000
     else:
-        no_paths = 1000000
+        no_paths = 500000
         no_time_steps = 100
 
     rnd_generator.set_seed(seed)
@@ -83,7 +83,7 @@ for i in range(0, no_dt_s):
                  "vol_swap_approx": str(vol_swap_approx[i]), "variance_swap": str(variance_swap[i]),
                  "out_variance_swap": str(output_variance_swap[i]), "out_vol_swap": str(output_vol_swap[i])})
 
-file = open('D://GitHubRepository//Python//SV_Engines//Examples//Chapter6//output_rbergomi_h_03_rho__06.csv', 'w')
+file = open('D://GitRepository//Python//SV_Engines//Examples//Chapter6//output_rbergomi_h_03_rho_0.csv', 'w')
 csv_writer = csv.DictWriter(file, fieldnames=headers, lineterminator='\n')
 csv_writer.writeheader()
 csv_writer.writerows(rows)

MC_Engines/MC_RBergomi/ToolsVariance.py
@@ -10,12 +10,14 @@ from math import gamma
 def get_volterra_covariance(s: float, t: float, h: float):
     # We suppose that t > s
     if s > 0.0:
-        # gamma = 0.5 - h
-        x = s / t
-        # alpha = ((1.0 - 2.0 * gamma) / (1.0 - gamma)) * np.power(x, gamma) * hyp2f1(1.0, gamma, 2.0 - gamma, x)
-        # return np.power(s, 2.0 * h) * alpha
-        alpha = 2.0 * np.power(s, h + 0.5) * np.power(t, h - 0.5) * h / (h + 0.5)
-        return alpha * hyp2f1(0.5 - h, 1.0, h + 1.5, x)
+        if s < t:
+            x = s / t
+            alpha = 2.0 * np.power(s, h + 0.5) * np.power(t, h - 0.5) * h / (h + 0.5)
+            return alpha * hyp2f1(0.5 - h, 1.0, h + 1.5, x)
+        else:
+            x = t / s
+            alpha = 2.0 * np.power(t, h + 0.5) * np.power(s, h - 0.5) * h / (h + 0.5)
+            return alpha * hyp2f1(0.5 - h, 1.0, h + 1.5, x)
 
     else:
         return 0.0
@@ -77,7 +79,7 @@ def get_covariance_matrix(t_i_s: ndarray, h: float, rho: float):
 
     for i in range(0, no_time_steps):
         for j in range(0, i + 1):
-            cov[i + no_time_steps, j + no_time_steps] = get_volterra_covariance(t_i_s[j], t_i_s[i], h)
+            cov[i + no_time_steps, j + no_time_steps] = get_volterra_covariance(t_i_s[i], t_i_s[j], h)
             # cov[i + no_time_steps, j + no_time_steps] = get_fbm_covariance(t_i_s[j], t_i_s[i], h)
             cov[j + no_time_steps, i + no_time_steps] = cov[i + no_time_steps, j + no_time_steps]
 