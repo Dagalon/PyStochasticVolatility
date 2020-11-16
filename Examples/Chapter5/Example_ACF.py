# Fixed bug covariance matrix

Examples/Chapter5/Example_ACF.py
@@ -6,7 +6,7 @@ from FractionalBrownian import fBM
 from statsmodels.graphics.tsaplots import plot_acf
 
 # simulation info
-h_s = [0.1, 0.3, 0.6, 0.8]
+h = 0.6
 no_time_steps = 2000
 T = 1.0
 d_t = np.linspace(0.0, T, no_time_steps)
@@ -15,12 +15,8 @@ seed = 2357575
 # random number generator
 rnd_generator = RNG.RndGenerator(seed)
 
-fig, axs = plt.subplots(4, 1)
-
-for i in range(0, len(h_s)):
-    rnd_generator.set_seed(seed)
-    path = fBM.truncated_fbm(0.0, T, 0.0, rnd_generator, h_s[i], 1, no_time_steps)
-    axs[i].plot(d_t, path[0, :].reshape(d_t.shape), color='black')
-    axs[i].set_title('H=' + str(h_s[i]))
+rnd_generator.set_seed(seed)
+path = fBM.truncated_fbm(0.0, T, 0.0, rnd_generator, h, 1, no_time_steps)
+plot_acf(np.diff(path[0, :].reshape(d_t.shape)), lags=30, title='H=' + str(h))
 
 plt.show()

Examples/Chapter6/Example_6_5_19.py
@@ -21,7 +21,7 @@ sigma_0 = np.sqrt(v0)
 
 parameters = [nu, rho, h]
 
-seed = 123456789
+seed = 123
 # no_paths = 10
 
 # delta_time = 1.0 / 365.0
@@ -51,14 +51,14 @@ variance_swap_mc = []
 for i in range(0, no_dt_s):
     if dt[i] < 0.1:
         no_time_steps = 100
-        no_paths = 1000000
+        no_paths = 1
     else:
         no_time_steps = 100
-        no_paths = 1000000
+        no_paths = 1
 
     rnd_generator.set_seed(seed)
     map_output = RBergomi_Engine.get_path_multi_step(0.0, dt[i], parameters, f0, sigma_0, no_paths,
-                                                     no_time_steps, Types.TYPE_STANDARD_NORMAL_SAMPLING.REGULAR_WAY,
+                                                     no_time_steps, Types.TYPE_STANDARD_NORMAL_SAMPLING.ANTITHETIC,
                                                      rnd_generator)
 
     # mc_option_price = options[i].get_price(map_output[Types.RBERGOMI_OUTPUT.PATHS][:, -1])
@@ -67,11 +67,11 @@ for i in range(0, no_dt_s):
 
     implied_vol_atm.append(implied_volatility(mc_option_price[0], f0, f0, dt[i], 0.0, 0.0, 'c'))
     vol_swap_mc.append(np.mean(np.sqrt(np.sum(map_output[Types.RBERGOMI_OUTPUT.INTEGRAL_VARIANCE_PATHS], 1) / dt[i])))
+    implied_vol_approx.append(
+        ExpansionTools.get_iv_atm_rbergomi_approximation(parameters, vol_swap_mc[i], sigma_0, dt[i], 'vol_swap'))
     error_mc_vol_swap = np.std(
         np.sqrt(np.sum(map_output[Types.RBERGOMI_OUTPUT.INTEGRAL_VARIANCE_PATHS], 1) / dt[i])) / np.sqrt(no_paths)
     vol_swap_approx.append(ExpansionTools.get_vol_swap_rbergomi(parameters, sigma_0, dt[i]))
-    implied_vol_approx.append(
-        ExpansionTools.get_iv_atm_rbergomi_approximation(parameters, vol_swap_mc[i], sigma_0, dt[i], 'vol_swap'))
     variance_swap.append(ExpansionTools.get_variance_swap_rbergomi(parameters, sigma_0, dt[i]))
     variance_swap_mc.append(np.sqrt(np.mean(np.sum(map_output[Types.RBERGOMI_OUTPUT.INTEGRAL_VARIANCE_PATHS], 1) / dt[i])))
     output_vol_swap.append(implied_vol_atm[i] - vol_swap_mc[i])
@@ -87,7 +87,7 @@ for i in range(0, no_dt_s):
                  "vol_swap_approx": str(vol_swap_approx[i]), "variance_swap": str(variance_swap[i]),
                  "out_variance_swap": str(output_variance_swap[i]), "out_vol_swap": str(output_vol_swap[i])})
 
-file = open('D://GitHubRepository//Python//SV_Engines//Examples//Chapter6//output_rbergomi_h_03_rho_0.csv', 'w')
+file = open('D://GitRepository//Python//SV_Engines//Examples//Chapter6//output_rbergomi_h_05_rho_06.csv', 'w')
 csv_writer = csv.DictWriter(file, fieldnames=headers, lineterminator='\n')
 csv_writer.writeheader()
 csv_writer.writerows(rows)

MC_Engines/MC_RBergomi/ToolsVariance.py
@@ -57,7 +57,7 @@ def get_covariance_w_v_w_t(s: float, t: float, rho: float, h: float):
     return rho * d_h * (np.power(s, h + 0.5) - (np.power(s - np.minimum(s, t), h + 0.5)))
 
 
-# @nb.jit("f8[:,:](f8[:], f8, f8)", nopython=True, nogil=True)
+@nb.jit("f8[:,:](f8[:], f8, f8)", nopython=True, nogil=True)
 def get_covariance_matrix(t_i_s: ndarray, h: float, rho: float):
     no_time_steps = len(t_i_s)
     cov = np.zeros(shape=(2 * no_time_steps, 2 * no_time_steps))
@@ -69,11 +69,11 @@ def get_covariance_matrix(t_i_s: ndarray, h: float, rho: float):
     for i in range(0, no_time_steps):
         for j in range(no_time_steps, 2 * no_time_steps):
             cov[i, j] = get_covariance_w_v_w_t(t_i_s[j - no_time_steps], t_i_s[i], rho, h)
-            # cov[j, i] = cov[i, j]
+            cov[j, i] = cov[i, j]
 
-    for i in range(no_time_steps, 2 * no_time_steps):
-        for j in range(0, no_time_steps):
-            cov[i, j] = get_covariance_w_v_w_t(t_i_s[j], t_i_s[i - no_time_steps], rho, h)
+    # for i in range(no_time_steps, 2 * no_time_steps):
+    #     for j in range(0, no_time_steps):
+    #         cov[i, j] = get_covariance_w_v_w_t(t_i_s[i - no_time_steps], t_i_s[j], rho, h)
 
     for i in range(0, no_time_steps):
         # for j in range(0, i + 1):
@@ -123,6 +123,7 @@ def generate_paths_rbergomi(s0: float,
                                                            nu * d_w_i_h)
             int_v_t[k, j - 1] = delta_i_s * 0.5 * (sigma_i_1[k, j - 1] * sigma_i_1[k, j - 1] +
                                                    sigma_i_1[k, j] * sigma_i_1[k, j])
+            # int_v_t[k, j - 1] = delta_i_s * sigma_i_1[k, j - 1] * sigma_i_1[k, j - 1]
             paths[k, j] = paths[k, j - 1] * np.exp(- 0.5 * int_v_t[k, j - 1] +
                                                    sigma_i_1[k, j - 1] * d_w_i_s)
 