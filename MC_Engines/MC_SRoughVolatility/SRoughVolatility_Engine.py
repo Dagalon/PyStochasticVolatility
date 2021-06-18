# New example for super rough process. We must fix the covariance matrix.

MC_Engines/MC_SRoughVolatility/SRoughVolatility_Engine.py
@@ -1,7 +1,7 @@
 import numba as nb
 import numpy as np
 
-from Tools.Types import ndarray
+from Tools.Types import ndarray, Vector,  TYPE_STANDARD_NORMAL_SAMPLING, RBERGOMI_OUTPUT
 from MC_Engines.MC_SRoughVolatility import ToolsVariance
 from Tools import AnalyticTools
 
@@ -102,3 +102,56 @@ def generate_paths_normal_super_rough(s0: float,
             w_i_h_1 = w_t_k[j + no_time_steps - 2]
 
     return paths, sigma_i_1, int_v_t
+
+
+def get_path_exp_multi_step(t0: float,
+                            t1: float,
+                            parameters: Vector,
+                            f0: float,
+                            sigma_0: float,
+                            no_paths: int,
+                            no_time_steps: int,
+                            type_random_number: TYPE_STANDARD_NORMAL_SAMPLING,
+                            rnd_generator,
+                            **kwargs) -> map:
+    nu = parameters[0]
+    rho = parameters[1]
+    beta = parameters[2]
+
+    no_paths = 2 * no_paths if type_random_number == TYPE_STANDARD_NORMAL_SAMPLING.ANTITHETIC else no_paths
+
+    t_i_s = np.array(get_time_steps(t0, t1, no_time_steps, **kwargs))
+    no_time_steps = len(t_i_s)
+
+    s_t = np.empty((no_paths, no_time_steps))
+    s_t[:, 0] = f0
+
+    z_i_s = rnd_generator.normal(mu=0.0, sigma=1.0, size=(2 * (no_time_steps - 1), no_paths),
+                                 sampling_type=type_random_number)
+    map_out_put = {}
+    outputs = generate_paths_exp_super_rough(f0,
+                                             sigma_0,
+                                             nu,
+                                             beta,
+                                             z_i_s,
+                                             np.linalg.cholesky(ToolsVariance.get_covariance_matrix(t_i_s[1:], beta, rho)),
+                                             t_i_s,
+                                             no_paths)
+
+    map_out_put[RBERGOMI_OUTPUT.PATHS] = outputs[0]
+    map_out_put[RBERGOMI_OUTPUT.SPOT_VOLATILITY_PATHS] = outputs[1]
+    map_out_put[RBERGOMI_OUTPUT.VARIANCE_SPOT_PATHS] = outputs[1] * outputs[1]
+    map_out_put[RBERGOMI_OUTPUT.INTEGRAL_VARIANCE_PATHS] = outputs[2]
+    map_out_put[RBERGOMI_OUTPUT.TIMES] = t_i_s
+
+    return map_out_put
+
+
+def get_time_steps(t0: float, t1: float, no_time_steps: int, **kwargs):
+    if len(kwargs) > 0:
+        extra_points = kwargs['extra_sampling_points']
+        basis_sampling_dates = np.linspace(t0, t1, no_time_steps).tolist()
+        full_points = np.array(list(set(extra_points + basis_sampling_dates)))
+        return sorted(full_points)
+    else:
+        return np.linspace(t0, t1, no_time_steps)

MC_Engines/MC_SRoughVolatility/ToolsVariance.py
@@ -4,16 +4,20 @@ import numba as nb
 from scipy.integrate import quad_vec, quad, quadrature
 from Tools.Types import ndarray
 from functools import partial
-from ncephes import hyp2f1
 
 
-# @nb.jit("f8[:](f8[:], f8)", nopython=True, nogil=True)
+@nb.jit("f8[:](f8[:], f8)", nopython=True, nogil=True)
 def get_variance(t: ndarray, beta: float):
     return np.power(-np.log(t), -beta)
 
 
+@nb.jit("f8(f8, f8)", nopython=True, nogil=True)
+def get_kernel(x, beta):
+    return np.sqrt(x * np.power(-np.log(x), beta + 1.0))
+
+
 @nb.jit("f8(f8, f8, f8, f8)", nopython=True, nogil=True)
-def get_kernel(u, s, t, beta):
+def get_kernel_cov(u, s, t, beta):
     if s < t:
         d_t_s = (t - s)
         return np.power((d_t_s + u) * u, -0.5) * np.power(np.log(u) * np.log(d_t_s + u), -0.5 * (beta + 1.0))
@@ -23,35 +27,42 @@ def get_kernel(u, s, t, beta):
 
 
 def get_volterra_covariance(s: float, t: float, beta: float):
-    # epsilon = 1.0e-03
     if s > 0.0 and t > 0.0:
-        f_kernel = partial(get_kernel, s=s, t=t, beta=beta)
+        f_kernel = partial(get_kernel_cov, s=s, t=t, beta=beta)
         min_t_s = np.minimum(t, s)
         integral_value = quad(f_kernel, 0.0, min_t_s)
-
-        # Approximation for the singularity
-        # partial_integral_value = quad(f_kernel, min_t_s * epsilon, min_t_s)
-        # if s > t:
-        #     d_s_t = (s - t)
-        #     alpha = (2.0 * np.sqrt(epsilon * t) / 3.0) * np.power(d_s_t, - 0.5) * np.power(-np.log(d_s_t),
-        #                                                                                    - 0.5 * (beta + 1.0))
-        #
-        #     aux_value = (partial_integral_value[0] + alpha * hyp2f1((beta + 1.0) * 0.5, 0.5, 1.5, min_t_s * epsilon)) * beta
-        #
-        # elif s < t:
-        #     d_s_t = (t - s)
-        #     alpha = (2.0 * np.sqrt(epsilon * s) / 3.0) * np.power(d_s_t, - 0.5) * np.power(-np.log(d_s_t),
-        #                                                                                    - 0.5 * (beta + 1.0))
-        #
-        #     aux_value = (partial_integral_value[0] + alpha * hyp2f1((beta + 1.0) * 0.5, 0.5, 1.5, min_t_s * epsilon)) * beta
-        #
-        # else:
-        #
-        #     aux_value = partial_integral_value[0] * beta + np.power(-np.log(min_t_s * epsilon), - beta)
-
-        # return (partial_integral_value[0] + alpha * hyp2f1((beta + 1.0) * 0.5, 0.5, 1.5, min_t_s * epsilon)) * beta
 
         return integral_value[0] * beta
 
     else:
         return 0.0
+
+
+def get_covariance_w_v_w_t(s: float, t: float, rho: float, beta: float):
+    if s > 0.0 and t > 0.0:
+        f_kernel = partial(get_kernel, beta=beta)
+        max_s_t = np.maximum(t - s, 0.0)
+        integral_value = quad(f_kernel, 0.0, max_s_t)
+        return np.sqrt(beta) * rho * integral_value[0]
+    else:
+        return 0.0
+
+
+def get_covariance_matrix(t_i_s: ndarray, beta: float, rho: float):
+    no_time_steps = len(t_i_s)
+    cov = np.zeros(shape=(2 * no_time_steps, 2 * no_time_steps))
+
+    for i in range(0, no_time_steps):
+        for j in range(0, no_time_steps):
+            cov[i, j] = np.minimum(t_i_s[i], t_i_s[j])
+
+    for i in range(0, no_time_steps):
+        for j in range(no_time_steps, 2 * no_time_steps):
+            cov[i, j] = get_covariance_w_v_w_t(t_i_s[j - no_time_steps], t_i_s[i], rho, beta)
+            cov[j, i] = cov[i, j]
+
+    for i in range(0, no_time_steps):
+        for j in range(0, no_time_steps):
+            cov[i + no_time_steps, j + no_time_steps] = get_volterra_covariance(t_i_s[i], t_i_s[j], beta)
+
+    return cov