6372f56f2082fc555ed340c8373e5d43f4a6546a
Dagalon <david.garcia.lorite@outlook.com>
2021-12-13 09:26:14 +0100

    # Fixed bugs in watanabe's expansion

--- AnalyticEngines/BetaZeroSabr/EuropeanOptionExpansion.py
+++ AnalyticEngines/BetaZeroSabr/EuropeanOptionExpansion.py
@@ -1,5 +1,4 @@
 import numpy as np
-import numba as nb
 from scipy.special import ndtr
 from Tools.AnalyticTools import normal_pdf
 from Tools.Bachelier import binary_flag
@@ -12,10 +11,13 @@ def CallOptionWatanabeExpansion(f, k, t, alpha, nu, rho):
     rho_inv = np.sqrt(1.0 - rho * rho)
 
     g_y = phi_y - y * ndtr(-y)
-    first_term = 0.5 * np.sqrt(t) * phi_y * (rho * nu * y)
-    second_term = t * phi_y * (np.power(nu * rho, 2.0) * ((3.0 * np.power(y, 4.0) - 12.0 * np.power(y, 2.0) - 1.0) / 24.0) + np.power(nu * rho_inv, 2.0) * (4.0 * np.power(y, 2.0) - 6.0 / 24.0))
+    g1 = 0.5 * np.sqrt(t) * phi_y * (rho * nu * y)
+    g21 = 0.5 * t * phi_y * np.power(nu * rho, 2.0) * (np.power(rho, 2.0) * (y - 3.0) + 2.0) - 0.25 * phi_y * nu * nu * t
+    g22 = 0.5 * phi_y * t * (np.power(nu * rho_inv, 2.0) * (y * y / 3.0 + 1.0 / 6.0) +
+                             np.power(nu * rho, 2.0) * (1.5 * np.power(rho_inv * y, 2.0) + 0.25 * np.power(y, 4.0))) + \
+        0.375 * nu * nu * phi_y * np.power(rho_inv, 4.0) * t
 
-    price = alpha * sqrt_t * (g_y + first_term + second_term)
+    price = alpha * sqrt_t * (g_y + g1 + g21 + g22)
     return price
 
 
--- AnalyticEngines/BetaZeroSabr/ExampleCallOptionPrice.py
+++ AnalyticEngines/BetaZeroSabr/ExampleCallOptionPrice.py
@@ -4,7 +4,7 @@ from Instruments.EuropeanInstruments import EuropeanOption, TypeSellBuy, TypeEur
 from Tools import RNG, Types
 from AnalyticEngines.BetaZeroSabr import EuropeanOptionExpansion
 from VolatilitySurface.Tools import SABRTools
-from Tools.Bachelier import bachelier
+from Tools.Bachelier import bachelier, implied_volatility
 
 # option info
 f0 = 0.01
@@ -14,7 +14,7 @@ t = 1.0
 # sabr parameters
 alpha = 0.3
 nu = 0.7
-rho = -0.6
+rho = 0.0
 parameters = [alpha, nu, rho]
 
 # Antonov price
@@ -22,8 +22,8 @@ antonov_option_price = CallOptionPrice(t, f0, strike, alpha, nu, rho)
 
 # watanabe expansion
 watanabe_price = EuropeanOptionExpansion.EuropeanOptionPrice(f0, strike, t, alpha, nu, rho, 'c', "Watanabe")
-
-#hagan
+iv_watanabe = implied_volatility(watanabe_price, f0, strike, t, 'c')
+# Hagan
 iv_hagan = SABRTools.sabr_normal_jit(f0, strike, alpha, rho, nu, t)
 hagan_price = bachelier(f0, strike, t, iv_hagan, 'c')
 
@@ -40,4 +40,4 @@ map_output = SABR_Normal_Engine.get_path_multi_step(0.0, t, parameters, f0, no_p
 
 mc_option_price = option.get_price(map_output[Types.SABR_OUTPUT.PATHS][:, -1])
 mc_price = mc_option_price[0]
-
+iv_mc = implied_volatility(mc_price, f0, strike, t, 'c')
