4a88bb7b44e1ea42648cf0c938c5080434ae17b5
Dagalon <david.garcia.lorite@outlook.com>
2022-04-06 17:07:58 +0200

    # new example local vol sabr and new expansion for a call option under local vol equivalent dynamic

--- AnalyticEngines/BetaZeroSabr/Examples/LocalVolSabr.py
+++ AnalyticEngines/BetaZeroSabr/Examples/LocalVolSabr.py
@@ -11,9 +11,9 @@ from Solvers.PDE_Solver.Types import BoundaryConditionType, np_ndarray, SchemeTy
 from Solvers.PDE_Solver.TerminalConditions import TerminalCondition
 from Solvers.PDE_Solver.BoundariesConditions import Zero_Laplacian_BC
 from VolatilitySurface.Tools import SABRTools
-from Tools.Bachelier import implied_volatility
 from AnalyticEngines.BetaZeroSabr import ExpansionTools
 from functools import partial
+from Tools.Bachelier import bachelier
 
 
 def call(mesh: Mesh, k: float) -> np_ndarray:
@@ -28,8 +28,8 @@ strikes = [f0 + si / 10000.0 for si in spreads]
 
 # sabr parameters
 alpha = 0.007
-nu = 0.3
-rho = 0.6
+nu = 0.6
+rho = -0.6
 parameters = [alpha, nu, rho]
 
 # meshes
@@ -50,7 +50,10 @@ operator_impl = PDEOperators.LinearPDEOperator(mesh_x, pde, bc)
 operators = [operator_exp, operator_impl]
 
 
-iv_pde = []
+pde_price = []
+hagan_price = []
+watanabe_price_lv = []
+watanabe_price_sv = []
 
 for i in range(0, len(strikes)):
     pd_solver = PDESolvers.FDSolver(mesh_t,
@@ -63,21 +66,35 @@ for i in range(0, len(strikes)):
     pd_solver.solver()
     f = interp1d(mesh_x.get_points(), pd_solver._u_grid[:, 0], kind='linear', fill_value='extrapolate')
     price = float(f(f0))
-    iv = implied_volatility(price, f0, strikes[i], t, 'c')
-    iv_pde.append(iv)
+    pde_price.append(price)
 
-iv_hagan = []
-iv_watanabe = []
-for i in range(0, len(strikes)):
-    iv = SABRTools.sabr_normal_jit(f0, strikes[i], alpha, rho, nu, t)
-    iv_hagan.append(iv)
-    iv = ExpansionTools.get_iv_normal_sabr_watanabe_expansion(f0, strikes[i], t, alpha, nu, rho)
-    iv_watanabe.append(iv)
+    #hagan price
+    iv_hagan = SABRTools.sabr_normal_jit(f0, strikes[i], alpha, rho, nu, t)
+    hagan_price.append(bachelier(f0, strikes[i], t, iv_hagan, 'c'))
 
+    #watanabe price
+    price_sv = ExpansionTools.get_option_normal_sabr_watanabe_expansion(f0, strikes[i], t, alpha, nu, rho, 'c')
+    price_lv = ExpansionTools.get_option_normal_sabr_loc_vol_expansion(f0, strikes[i], t, alpha, nu, rho, 'c')
+    watanabe_price_lv.append(price_lv)
+    watanabe_price_sv.append(price_sv)
 
-plt.plot(strikes, iv_hagan, label='IV Hagan', linestyle='dotted')
-plt.plot(strikes, iv_pde, label='IV Local Vol SABR', linestyle='dashed')
-plt.plot(strikes, iv_watanabe, label='IV Watanabe', linestyle='dashed')
+plt.plot(strikes, hagan_price, label='Hagan price', linestyle='dotted')
+# plt.plot(strikes, watanabe_price_sv, label='Watanabe price SV', linestyle='dashed')
+plt.plot(strikes, watanabe_price_lv, label='Watanabe price LV', linestyle='-')
+plt.plot(strikes, pde_price, label='PDE price', linestyle=':')
+
+# iv_hagan = []
+# iv_watanabe = []
+# for i in range(0, len(strikes)):
+#     iv = SABRTools.sabr_normal_jit(f0, strikes[i], alpha, rho, nu, t)
+#     iv_hagan.append(iv)
+#     iv = ExpansionTools.get_iv_normal_sabr_watanabe_expansion(f0, strikes[i], t, alpha, nu, rho)
+#     iv_watanabe.append(iv)
+#
+#
+# plt.plot(strikes, iv_hagan, label='IV Hagan', linestyle='dotted')
+# plt.plot(strikes, iv_pde, label='IV Local Vol SABR', linestyle='dashed')
+# plt.plot(strikes, iv_watanabe, label='IV Watanabe', linestyle='dashed')
 
 plt.legend()
 plt.show()
--- AnalyticEngines/BetaZeroSabr/ExpansionTools.py
+++ AnalyticEngines/BetaZeroSabr/ExpansionTools.py
@@ -1,11 +1,12 @@
 import numpy as np
-from ncephes import ndtr
-
 import Tools.AnalyticTools
+
+from ncephes import ndtr
+from MC_Engines.MC_LocalVol import LocalVolFunctionals
 
 
 def G(y):
-    return Tools.AnalyticTools.normal_pdf(0.0, 1.0, y) + y * ndtr(-y)
+    return Tools.AnalyticTools.normal_pdf(0.0, 1.0, y) - y * ndtr(-y)
 
 
 def get_option_normal_sabr_watanabe_expansion(f0, k, t, alpha, nu, rho, option_type):
@@ -16,7 +17,7 @@ def get_option_normal_sabr_watanabe_expansion(f0, k, t, alpha, nu, rho, option_t
 
     a_t = 0.5 * rho * nu * y
     b_t = 0.5 * np.power(nu * rho, 2.0) * (np.power(y, 2.0) / 3.0 + 1.0 / 6.0) + \
-          0.5 * np.power(nu * rho, 2.0) * np.power(0.25 * (y * y - 1.0), 2.0) + \
+          0.5 * np.power(nu * rho, 2.0) * np.power(0.5 * (y * y - 1.0), 2.0) + \
           0.5 * np.power(rho_inv * nu, 2.0) * ((np.power(y, 2.0) + 2.0) / 3.0) - 0.25 * nu * nu
 
     if option_type == 'c':
@@ -34,3 +35,26 @@ def get_iv_normal_sabr_watanabe_expansion(f0, k, t, alpha, nu, rho):
           0.5 * np.power(rho_inv * nu, 2.0) * ((np.power(y, 2.0) + 2.0) / 3.0) - 0.25 * nu * nu
 
     return alpha * (1.0 + np.sqrt(t) * a_t + t * b_t)
+
+
+def get_option_normal_sabr_loc_vol_expansion(f0, k, t, alpha, nu, rho, option_type):
+    x = np.array([f0])
+    sigma_0 = LocalVolFunctionals.local_vol_normal_sabr(t, x, f0, alpha, rho, nu)[0]
+    y = (k - f0) / (sigma_0 * np.sqrt(t))
+    phi_y = Tools.AnalyticTools.normal_pdf(0.0, 1.0, y)
+    g_y = G(y)
+    rho_inv = 1.0 - rho * rho
+
+    a_t = 0.5 * rho * nu * y
+    b_t = rho * nu * alpha * (2.0 * y * y + 1.0) / 6.0 + 0.25 * nu * nu * rho_inv + 0.125 * np.power(rho * nu, 2.0) * np.power(y * y - 1, 2.0)
+    c_t = alpha * np.power(rho * nu, 2.0) * (0.25 * np.power(y, 5.0) - np.power(y, 3.0) / 3.0 + 0.25 * y)
+
+    if option_type == 'c':
+        return alpha * np.sqrt(t) * (g_y + phi_y * np.sqrt(t) * a_t + phi_y * t * b_t + phi_y * np.power(t, 1.5) * c_t)
+    else:
+        return alpha * np.sqrt(t) * (g_y + phi_y * np.sqrt(t) * a_t + phi_y * t * b_t + phi_y * np.power(t, 1.5) * c_t) - (f0 - k)
+
+
+
+
+
