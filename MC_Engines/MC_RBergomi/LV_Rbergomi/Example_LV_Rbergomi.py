11809d6d2aba02052b8b0085b8032e228002ba08
Dagalon <63908589+Dagalon@users.noreply.github.com>
2022-05-23 06:45:29 +0200

    # local vol for x_t = log(s_t)

--- MC_Engines/MC_RBergomi/LV_Rbergomi/Example_LV_Rbergomi.py
+++ MC_Engines/MC_RBergomi/LV_Rbergomi/Example_LV_Rbergomi.py
@@ -17,7 +17,7 @@ sigma_0 = np.sqrt(v0)
 parameters = [nu, rho, h]
 
 f0 = 100
-T = np.arange(0.1, 182, 2) * 1.0 / 365.0
+T = np.arange(7, 90, 2) * 1.0 / 365.0
 
 seed = 123456789
 
@@ -27,16 +27,18 @@ no_paths = 500000
 # Compute the conditional expected by kernel estimators
 delta = 0.01
 x = np.zeros(3)
-x[0] = (1.0 - delta) * f0
-x[1] = f0
-x[2] = (1.0 + delta) * f0
+x[0] = np.log(f0) - delta
+x[1] = np.log(f0)
+x[2] = np.log(f0) + delta
 
-rnd_generator = RNG.RndGenerator(seed)
 atm_lv = []
 atm_lv_skew = []
 atm_lv_skew_derive_estimator = []
 
+rnd_generator = RNG.RndGenerator(seed)
+
 for t_i in T:
+    rnd_generator.set_seed(seed)
     # Julien Guyon paper https://papers.ssrn.com/sol3/papers.cfm?abstract_id=1885032&download=yes
     h = 1.5 * np.sqrt(t_i) * np.power(no_paths, - 0.2)
 
@@ -47,18 +49,19 @@ for t_i in T:
 
 
     gaussian_estimator_t = NonParametricEstimatorSLV.gaussian_kernel_estimator_slv(map_bergomi_output[Types.RBERGOMI_OUTPUT.VARIANCE_SPOT_PATHS][:, -1],
-                                                                                   map_bergomi_output[Types.RBERGOMI_OUTPUT.PATHS][:, -1],
+                                                                                   np.log(map_bergomi_output[Types.RBERGOMI_OUTPUT.PATHS][:, -1]),
                                                                                    x,
                                                                                    h)
     skew_gaussian_estimator_t = NonParametricEstimatorSLV.gaussian_kernel_estimator_skew_slv(
         map_bergomi_output[Types.RBERGOMI_OUTPUT.VARIANCE_SPOT_PATHS][:, -1],
-        map_bergomi_output[Types.RBERGOMI_OUTPUT.PATHS][:, -1],
-        np.asfortranarray(f0),
+        np.log(map_bergomi_output[Types.RBERGOMI_OUTPUT.PATHS][:, -1]),
+        np.asfortranarray(np.log(f0)),
         h)
 
-    vol = np.interp(np.asfortranarray(f0), x, gaussian_estimator_t)
-    atm_lv.append(gaussian_estimator_t[1])
-    atm_lv_skew.append(0.5 * (gaussian_estimator_t[2] - gaussian_estimator_t[0]) / delta)
+    atm_lv.append(np.sqrt(gaussian_estimator_t[1]))
+    skew = 0.5 * (gaussian_estimator_t[2] - gaussian_estimator_t[0]) / delta
+    # atm_lv_skew.append(skew)
+    atm_lv_skew.append(0.5 * skew / np.sqrt(gaussian_estimator_t[1]))
     # atm_lv_skew_derive_estimator.append(skew_gaussian_estimator_t[0])
 
 
@@ -66,12 +69,13 @@ def f_law(x, b, c):
     return b * np.power(x, c)
 
 
-popt_atm_lv_skew, pcov_diff_vols_swap = curve_fit(f_law, T, atm_lv_skew)
+popt_atm_lv_skew, pcov_diff_vols_swap = curve_fit(f_law, T[1:], atm_lv_skew[1:])
 y_fit_atm_lv_skew = f_law(T, *popt_atm_lv_skew)
 
 
-plt.plot(x, atm_lv_skew, label="finite differences", color="black", linestyle="dotted")
-plt.plot(x, y_fit_atm_lv_skew, label="power law", color="black", linestyle="dotted")
+plt.plot(T[1:], atm_lv_skew[1:], label="atm skew LV", color="blue", linestyle="dotted")
+plt.plot(T[1:], y_fit_atm_lv_skew[1:], label=" %s * T^(%s)" % (round(popt_atm_lv_skew[0], 5),
+         round(popt_atm_lv_skew[1], 5)), color="green", linestyle="dotted")
 # plt.plot(x, atm_lv_skew_derive_estimator, label="derivative estimator", color="black", linestyle="dotted")
 plt.xlabel("T")
 plt.ylabel("d E(V_t|S_t=x) / dx")
