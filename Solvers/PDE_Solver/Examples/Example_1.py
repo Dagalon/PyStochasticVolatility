# fix bug in atm quadratic hagan's formula

Solvers/PDE_Solver/Examples/Example_1.py
@@ -18,15 +18,16 @@ try:
 except ImportError:
     print('The import of matplotlib is not working.')
 
-T = 4.0
-mesh_t = Mesh(uniform_mesh, 50, 0.0, T)
+T = 1.0
+mesh_t = Mesh(uniform_mesh, 10, 0.0, T)
 
-r = 0.03
-q = 0.01
+r = 0.0
+q = 0.0
 sigma = 0.3
 
 S0 = 100.0
-K = np.exp((r - q) * T) * S0 + 10
+# K = np.exp((r - q) * T) * S0 + 10
+K = 90.0
 log_K = np.log(K)
 
 f = np.exp((r - q) * T) * S0
@@ -38,7 +39,7 @@ end_time = time.time()
 print(end_time - start_time)
 print(analytic_price)
 
-mesh_x = LnUnderlyingMesh(r, q, sigma, S0, T, 0.999, uniform_mesh, 100)
+mesh_x = LnUnderlyingMesh(r, q, sigma, S0, T, 0.9999999, uniform_mesh, 100)
 
 bs_pde = PDE.from_ipde_terms(LN_BS_PDE(r, q, sigma))
 bc = Zero_Laplacian_BC()
@@ -67,5 +68,7 @@ f = interp1d(mesh_x.get_points(), pd_solver._u_grid[:, 0], kind='linear', fill_v
 pde_price = float(f(np.log(S0)))
 print(end_time - start_time)
 print(pde_price)
+
+
 
 

VolatilitySurface/Tools/SABRTools.py
@@ -85,8 +85,8 @@ def sabr_normal_partial_k_jit(f, k, alpha, rho, v, t):
     partial_k_phi = partial_k_psi / np.sqrt(1.0 - 2.0 * rho * psi + psi * psi)
     m = 1.0 + v * v * t * (2.0 - 3.0 * rho * rho) / 24.0
 
-    if np.abs(f-k) < 1.0e-06:
-        return alpha * m * rho * partial_k_psi
+    if np.abs(f - k) < 1.0e-06:
+        return 0.5 * rho * v * m
     else:
         phi = np.log((np.sqrt(1.0 - 2.0 * rho * psi + psi * psi) - rho + psi) / (1.0 - rho))
         return alpha * m * ((partial_k_psi / phi) - (partial_k_phi * psi) / (phi * phi))
@@ -101,10 +101,9 @@ def sabr_normal_quadratic_jit(f, k, alpha, rho, v, t):
 # @nb.jit("f8(f8,f8,f8,f8,f8,f8)", nopython=True, nogil=True)
 def sabr_normal_quadratic_partial_k_jit(f, k, alpha, rho, v, t):
     partial_k_normal_jit = sabr_normal_partial_k_jit(f, k, alpha, rho, v, t)
-    delta_k = 0.00001
-    der_aux = 0.5 * (sabr_normal_jit(f, f + delta_k, alpha, rho, v, t) - sabr_normal_jit(f, f - delta_k, alpha, rho, v, t)) / delta_k
-    # return partial_k_normal_jit * (1.0 + v * v * t / 6.0)
-    return der_aux
+    return partial_k_normal_jit * (1.0 + v * v * t / 6.0)
+    # return der_aux
+
 
 # @nb.jit("f8(f8,f8,f8,f8,f8)", nopython=True, nogil=True)
 def sabr_normal_quadratic_swap_vol_jit(f, alpha, rho, v, t):