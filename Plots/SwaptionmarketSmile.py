f11a7f29588e91422485c4b1f0af844318212fc6
Dagalon <63908589+Dagalon@users.noreply.github.com>
2024-04-01 22:32:32 +0200

    # new script to plot swaption smile

--- Plots/SwaptionmarketSmile.py
+++ Plots/SwaptionmarketSmile.py
@@ -1,39 +1,39 @@
-import numpy as np
 import matplotlib.pylab as plt
 import os
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 
 currentDirectory = os.path.dirname(os.path.realpath(__file__))
 folderDirectory = Path(currentDirectory)
 smileDataPath = os.path.join(folderDirectory, 'MarketData', 'swaption5Yx5Y.csv')
 
-smileDf = pd.read_csv(smileDataPath, header=None, names=["Maturity", "Strike", "Vol"], sep=";")
+smileDf = pd.read_csv(smileDataPath, header=None, names=["Maturity", "Forward", "Spread", "Vol", "Vol Market"], sep=";")
 maturities = smileDf.Maturity.unique()[1:]
 
 strikes = {}
 ivs = {}
+ivsMarket = {}
+spreads = {}
+forwards = {}
 
 for ti in maturities:
     subDf = smileDf.query(f"Maturity == '{ti}'")
-    strikes[ti] = list(subDf['Strike'].map(lambda x: float(x)))
+    spreads = list(subDf['Spread'].map(lambda x: float(x)))
+    forwards = list(subDf['Forward'].map(lambda x: float(x)))
+    strikes = list(np.sum(spreads, forwards))
     ivs[ti] = list(subDf['Vol'].map(lambda x: float(x)))
+    ivsMarket[ti] = list(subDf['VolMarket'].map(lambda x: float(x)))
 
+plot_to_print = 2
 
-fig, axs = plt.subplots(1, 3)
-fig.suptitle('Swaption 5Yx5Y Smile')
+plt.plot(strikes[maturities[plot_to_print]], ivs[maturities[plot_to_print]], linestyle='dashed', color="blue",
+         label="SABR")
 
-# plot 1
-axs[0].set(xlabel='K', ylabel='IV')
-axs[0].plot(strikes[maturities[0]], ivs[maturities[0]], linestyle='dashed', color='blue')
-
-# plot 2
-axs[1].set(xlabel='K', ylabel='IV')
-axs[1].plot(strikes[maturities[1]], ivs[maturities[1]], linestyle='dashed', color='blue')
-
-# plot 3
-axs[2].set(xlabel='K', ylabel='IV')
-axs[2].plot(strikes[maturities[2]], ivs[maturities[2]], linestyle='dashed', color='blue')
-
+plt.scatter(strikes[maturities[plot_to_print]], ivsMarket[maturities[plot_to_print]], color="black", label="Market")
+plt.title("T=" + maturities[plot_to_print])
+plt.xlabel("K")
+plt.ylabel("IV")
+plt.legend()
 plt.show()
