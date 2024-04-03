73b632acdafcc9dfad44b35e0011ea5c22c27888
Dagalon <david.garcia.lorite@outlook.com>
2024-04-03 19:03:06 +0200

    # new plot smile swapption dynamic

--- Plots/SwaptionmarketSmile.py
+++ Plots/SwaptionmarketSmile.py
@@ -1,16 +1,16 @@
 import matplotlib.pylab as plt
 import os
 from pathlib import Path
+import operator
 
-import numpy as np
 import pandas as pd
 
 currentDirectory = os.path.dirname(os.path.realpath(__file__))
 folderDirectory = Path(currentDirectory)
-smileDataPath = os.path.join(folderDirectory, 'MarketData', 'swaption5Yx5Y.csv')
+smileDataPath = os.path.join(folderDirectory, 'MarketData', 'SwaptionTenor5Y.csv')
 
-smileDf = pd.read_csv(smileDataPath, header=None, names=["Maturity", "Forward", "Spread", "Vol", "Vol Market"], sep=";")
-maturities = smileDf.Maturity.unique()[1:]
+smileDf = pd.read_csv(smileDataPath, header=None, names=["FORWARD", "SPREAD", "MATURITY", "MARKET_VOLATILITY", "SABR_VOLATILITY"], sep=";")
+maturities = smileDf.MATURITY.unique()[1:]
 
 strikes = {}
 ivs = {}
@@ -19,21 +19,26 @@ spreads = {}
 forwards = {}
 
 for ti in maturities:
-    subDf = smileDf.query(f"Maturity == '{ti}'")
-    spreads = list(subDf['Spread'].map(lambda x: float(x)))
-    forwards = list(subDf['Forward'].map(lambda x: float(x)))
-    strikes = list(np.sum(spreads, forwards))
-    ivs[ti] = list(subDf['Vol'].map(lambda x: float(x)))
-    ivsMarket[ti] = list(subDf['VolMarket'].map(lambda x: float(x)))
+    subDf = smileDf.query(f"MATURITY == '{ti}'")
+    spreads[ti] = list(subDf['SPREAD'].map(lambda x: float(x)))
+    forwards[ti] = list(subDf['FORWARD'].map(lambda x: float(x)))
+    strikes[ti] = list(map(operator.add, spreads[ti], forwards[ti]))
+    ivs[ti] = list(subDf['SABR_VOLATILITY'].map(lambda x: float(x)))
+    ivsMarket[ti] = list(subDf['MARKET_VOLATILITY'].map(lambda x: float(x)))
 
 plot_to_print = 2
 
-plt.plot(strikes[maturities[plot_to_print]], ivs[maturities[plot_to_print]], linestyle='dashed', color="blue",
+plt.plot(strikes[maturities[plot_to_print]], ivs[maturities[plot_to_print]], linestyle='dashed', color="green",
          label="SABR")
+plt.scatter(strikes[maturities[plot_to_print]], ivsMarket[maturities[plot_to_print]], color="black", label="Market",
+            s=15, marker="x")
 
-plt.scatter(strikes[maturities[plot_to_print]], ivsMarket[maturities[plot_to_print]], color="black", label="Market")
+# plt.grid(linestyle='dashed')
+
 plt.title("T=" + maturities[plot_to_print])
 plt.xlabel("K")
 plt.ylabel("IV")
 plt.legend()
 plt.show()
+
+plt.savefig("D:/Latex/Thesis/Figures/Introducction/swaption_tenor_5Y_" + maturities[plot_to_print] + ".png")
