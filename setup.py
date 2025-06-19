b3c73ecbce00eb41c7f3b74acb2ef4d179fda54a
Dagalon <63908589+Dagalon@users.noreply.github.com>
2025-06-19 11:03:17 +0200

    Last version .whl

--- setup.py
+++ setup.py
@@ -1,19 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='PyStochasticVolatility',
-    version='1.0',
-    packages=['Tools', 'Solvers', 'Solvers.ODE_Solver', 'Solvers.PDE_Solver',
-              'Solvers.PDE_Solver.Examples', 'MCPricers', 'MC_Engines', 'MC_Engines.MC_SABR', 'MC_Engines.MC_Heston',
-              'MC_Engines.GenericSDE', 'MC_Engines.MC_RBergomi', 'MC_Engines.MC_MixedLogNormal',
-              'MC_Engines.MC_LocalVol', 'Instruments', 'AnalyticEngines',
-              'AnalyticEngines.FourierMethod', 'AnalyticEngines.FourierMethod.COSMethod',
-              'AnalyticEngines.FourierMethod.CharesticFunctions', 'AnalyticEngines.LocalVolatility',
-              'AnalyticEngines.LocalVolatility.Hagan', 'AnalyticEngines.LocalVolatility.Dupire', 'AnalyticEngines.VolatilityTools',
-              'AnalyticEngines.MalliavinMethod', 'VolatilitySurface', 'VolatilitySurface.Tools', 'FractionalBrownian'],
+    version='1.1',
+    # packages=['Tools', 'Solvers', 'Solvers.ODE_Solver', 'Solvers.PDE_Solver', 'Solvers.PDE_Solver.Examples', 'Examples', 'MCPricers', 'MC_Engines', 'MC_Engines.MC_SABR', 'MC_Engines.MC_Heston', 'MC_Engines.GenericSDE', 'MC_Engines.MC_Cheyette', 'MC_Engines.MC_LocalVol', 'MC_Engines.MC_RBergomi', 'MC_Engines.MC_MixedLogNormal', 'MC_Engines.MC_SRoughVolatility', 'Instruments', 'AnalyticEngines', 'AnalyticEngines.BetaZeroSabr', 'AnalyticEngines.FourierMethod', 'AnalyticEngines.FourierMethod.COSMethod', 'AnalyticEngines.FourierMethod.CharesticFunctions', 'AnalyticEngines.LocalVolatility', 'AnalyticEngines.LocalVolatility.Hagan', 'AnalyticEngines.LocalVolatility.Dupire', 'AnalyticEngines.MalliavinMethod', 'AnalyticEngines.VolatilityTools', 'VolatilitySurface', 'VolatilitySurface.Tools', 'FractionalBrownian'],
+    packages=find_packages(),
     url='https://github.com/Dagalon/PyStochasticVolatility.git',
     license='http://www.apache.org/licenses/LICENSE-2.0',
     author='David Garcia Lorite',
-    author_email='david.garcia.lorite@outlook.com',
-    description='Financial library creted to give support the book Malliavin Calculus and Stochastic Volatility.'
+    author_email='david.garcia.lorite@gmail.com',
+    description='Financial library created to give support the book Malliavin Calculus and Stochastic Volatility.'
 )
